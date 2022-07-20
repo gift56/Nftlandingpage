7bd5b6269246fec7aee2d14e73b8ded4ec318098
gift56 <efeasiughu@gmail.com>
2022-07-20 16:15:03 +0100

    done stying navbar

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Buttons = ({ text, blue }) => {
+const Buttons = ({ text, blue = false }) => {
     return (
         <Div>
             <button className={`${blue ? "blue" : ""}`}>{text}</button>
@@ -12,4 +12,20 @@ const Buttons = ({ text, blue }) => {
 export default Buttons;
 
 
-const Div = styled.div``;
+const Div = styled.div`
+  button {
+    border-radius: 4rem;
+    padding: 0.8rem 2rem;
+    border: none;
+    color: #fff;
+    font-size: 1.1rem;
+    cursor: pointer;
+    :not(.blue) {
+          background: transparent;
+          border: 1px solid #fff;
+    }
+   }
+   .blue {
+      background-color: #2d69fd;
+   }
+`;
