53efb7ca07f5d70ab74d97ac2ff76ccc7d07abd9
gift56 <efeasiughu@gmail.com>
2022-07-20 16:22:34 +0100

    add p tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar';
 import styled from 'styled-components';
+import Hero from '../assets/hero.png';
+import Herotext from '../assets/heroText.png';
 
 const Home = () => {
     return (
@@ -9,7 +11,16 @@ const Home = () => {
             <div className="ellipse"></div>
             <div className="container">
                 <div className="content">
-                    <h1>Explore Our Hero Text Digital NFT Market Place</h1>
+                    <h1>Explore Our Hero
+                        <span>
+                            <img src={Herotext} alt="heroText" />
+                        </span>
+                        Text Digital NFT Market Place
+                    </h1>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Ad eum dolor architecto assumenda perferendis temporibus.
+                    </p>
                 </div>
             </div>
         </Section>
