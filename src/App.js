366f54a8b91afdc68bd6a751f6b8f2fb0de2ed54
gift56 <efeasiughu@gmail.com>
2022-07-20 15:29:34 +0100

    sucess fully imported it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <Home />
     </div>
-  );
+  )
 }
 
 export default App;
