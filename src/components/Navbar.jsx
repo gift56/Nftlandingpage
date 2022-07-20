7a69dff9d3dfaa3cfcdc44ec42fce6e9ab7b0428
gift56 <efeasiughu@gmail.com>
2022-07-20 15:40:54 +0100

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+import Logo from '../assets/logo.png';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <Nav>
+            <div className="brand">
+                <img src={Logo} alt="logo" />
+            </div>
+            <div className="toggle"></div>
+            <div className="links">
+                <ul>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </Nav>
     )
 }
 
-export default Navbar
+export default Navbar;
+
+
+const Nav = styled.nav`
+
+`;
