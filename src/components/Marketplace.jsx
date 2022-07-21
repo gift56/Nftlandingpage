02a3f8ddc82393dd8956b0e8b4fc273ab83568d0
gift56 <efeasiughu@gmail.com>
2022-07-21 12:12:08 +0100

    added two new icons

---
+++ src/components/Marketplace.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+import { FaEthereum } from 'react-icons/fa';
+import { BsThreeDots } from 'react-icons/bs';
+
+const Marketplace = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Marketplace
