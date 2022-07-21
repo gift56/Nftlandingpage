fba81332f3003cda60250972da621de59929d95b
gift56 <efeasiughu@gmail.com>
2022-07-21 14:19:13 +0100

    working on fotter area

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { AiFillFacebook } from 'react-icons/ai';
+import { BsLinkedin } from 'react-icons/Bs';
+import { FaTwitterSquare, FaInstagramSquare } from 'react-icons/fa';
+import { MdEmail, MdPhoneInTalk } from 'react-icons/md';
+import { IoLocationSharp } from 'react-icons/ioS';
+
+const Footer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Footer
