b982d63c02439a6acd7acce5af7037598a5955ae
gift56 <efeasiughu@gmail.com>
2022-07-21 15:33:45 +0100

    done the on scrolling usestate

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -4,6 +4,10 @@ import { FaChevronUp } from 'react-icons/fa';
 
 const ScrollTop = () => {
     const [visible, setVisible] = useState(false);
+
+    window.addEventListener('scroll', () => {
+        window.pageYOffset > 100 ? setVisible(true) : setVisible(false);
+    });
     return (
         <div>
 
