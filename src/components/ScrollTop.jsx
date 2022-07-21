936df9eda4854422904fcc845027b4624d71f777
gift56 <efeasiughu@gmail.com>
2022-07-21 15:31:00 +0100

    working on scroll top

---
+++ src/components/ScrollTop.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FaChevronUp } from 'react-icons/fa';
+
+const ScrollTop = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ScrollTop
