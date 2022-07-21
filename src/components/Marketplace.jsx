ee39f26de8dbab042e2abb3d8dc0660a9546920c
gift56 <efeasiughu@gmail.com>
2022-07-21 12:13:46 +0100

    added needed img

--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import styled from 'styled-components';
 import { FaEthereum } from 'react-icons/fa';
 import { BsThreeDots } from 'react-icons/bs';
+import marketplace1 from '../assets/marketplace1.png';
+import marketplace2 from '../assets/marketplace2.png';
+import marketplace3 from '../assets/marketplace3.png';
+import marketplace4 from '../assets/marketplace4.png';
+import marketplace5 from '../assets/marketplace5.png';
+import marketplace6 from '../assets/marketplace6.png';
+import marketplace7 from '../assets/marketplace7.png';
+import marketplace8 from '../assets/marketplace8.png';
 
 const Marketplace = () => {
     return (
