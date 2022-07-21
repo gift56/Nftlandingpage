28e86fc700fdd931a88b6c1b9e87abfed6ea6fd8
gift56 <efeasiughu@gmail.com>
2022-07-21 09:39:14 +0100

    sucessfully imported clients

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Clients from "./components/Clients";
 import Home from "./components/Home";
 
 function App() {
-  document.title='Nft-Market'
+  document.title = 'Nft-Market'
   return (
     <div>
       <Home />
+      <Clients />
     </div>
   )
 }
