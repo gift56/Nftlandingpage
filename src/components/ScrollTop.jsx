a29ae95b74dc47ac7fcc3244dbaac4e80df032c1
gift56 <efeasiughu@gmail.com>
2022-07-21 15:31:57 +0100

    working on scrolling usesate

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { FaChevronUp } from 'react-icons/fa';
 
 const ScrollTop = () => {
+    const [visible, setVisible] = useState(false);
     return (
         <div>
 
