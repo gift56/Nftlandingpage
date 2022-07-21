ba2d9f86f1e80b5e83a564801da8f83088b62070
gift56 <efeasiughu@gmail.com>
2022-07-21 15:48:51 +0100

    added two icons on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo.png';
 import Buttons from './Buttons';
 
