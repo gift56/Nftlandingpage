29ea37f88ab5d5e9770ae78059c791e61704448d
gift56 <efeasiughu@gmail.com>
2022-07-21 09:49:21 +0100

    done with clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import client1 from '../assets/client1.png'
+import client2 from '../assets/client2.png'
+import client3 from '../assets/client3.png'
+import client4 from '../assets/client4.png'
+import client5 from '../assets/client5.png'
 
 const Clients = () => {
+    const clients = [client1, client2, client3, client4, client5]
     return (
-        <div>
-            Clients
-        </div>
+        <Section>
+            {clients.map((client, index) => (
+                <div className="clients" key={index}>
+                    <img src={client} alt="client" />
+                </div>
+            ))}
+        </Section>
     )
 }
 
-export default Clients
+export default Clients;
+
+
+const Section = styled.section`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin: 0 2rem;
+    margin-bottom: 5rem;
+`;
